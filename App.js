69c6c376fa35ea3d87008341ed914de149501080
Duncan <semunda9@gmail.com>
2020-03-24 17:54:35 +0200

    Added other routes for the library system

--- App.js
+++ App.js
@@ -99,9 +99,107 @@ app.get("/Client",function(req,res){
     res.render("Client");
    
 })
+// routes for book and book category
+app.post("/books",function(req,res){
+    const data1={
+        bookId:req.body.bookId,
+        bookTitle:req.body.title,
+        publishingHouse:req.body.publishingHouse,
+        dateOfPublication:req.body.dateOfPublication,
+        author:req.body.author,
+        pages:req.body.pages
+    }
 
+    connection.query("insert into book SET ? ",data1,function(error){
+        if(error)
+        res.send(error);
+        else
+        res.send("Done connection");
+    })
+});
 
+app.get("/Book",function(req,res){
+    res.render("Book");
+});
 
+//routes for bookCategory
+app.post("/BookCategories",function(req,res){
+    const data={
+        category_Id:req.body.categoryId,
+        category_Name:req.body.categoryName
+    }
+    connection.query("insert into book_category SET ?",data,function(error){
+        if(error)
+        res.send(error);
+        else
+        res.redirect("BookCategory")
+    })
+})
+
+app.get("/BookCategory",function(req,res){
+    connection.query("select * from book_category",(error,results)=>{
+        if(error)
+        res.send(error)
+        else
+        res.render("BookCategory",{items:results});
+    })
+    
+})
+// routes for checkin book
+
+app.get("/CheckIn",function(req,res){
+    connection.query("select firstName,lastName,bookTitle from Client,book",(error,results)=>{
+        if(error)
+        res.send(error)
+        else
+        res.render("CheckIn",{dropdowns:results})
+    })
+})
+
+app.post("/CheckIn",function(req,res){
+    const data={
+        firstName:req.body.names,
+        bookName:req.body.bookTitle
+
+    }
+    connection.query("insert into operations SET ?",data,(error)=>{
+if(error)
+res.send(error)
+else
+res.redirect("CheckIn")
+    })
+})
+
+//routes for Checkout 
+app.get("/CheckOut",function(req,res){
+    connection.query("select firstName,lastName,bookTitle from Client,book",(error,results)=>{
+        if(error)
+        res.send(error)
+        else
+        res.render("CheckOut",{dropdowns:results})
+    });
+    connection.query("select * from operations",(error,rows)=>{
+if(error)
+res.send(error)
+else 
+res.render("CheckOut",{items:rows})
+    })
+})
+
+app.post("/CheckOut",function(req,res){
+    const data={
+        firstName:req.body.names,
+        bookName:req.body.bookTitle
+
+    }
+    connection.query("insert into operations SET ?",data,(error)=>{
+if(error)
+res.send(error)
+else
+res.redirect("CheckOut")
+    })
+})
+//Listening port
 
 app.listen(3000,function(){
     console.log("server up");
